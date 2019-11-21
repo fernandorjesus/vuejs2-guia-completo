74f59f732602f4ea3bca3eccae4e26b9661648db
Fernando Jesus <fernandoj.5a@stelo.com.br>
2019-11-21 17:29:02 -0200

    Vue CLI

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ titulo }}</h1>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      titulo: "Olá Vue"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
