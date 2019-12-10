18b2a0dc18d31dc6ceda0137090b0ad555fe0a0a
Fernando Jesus <fernandoj.5a@stelo.com.br>
2019-12-10 17:42:40 -0200

    animações

---
+++ filtros-mixins/filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+    <hr>
+    <p>{{ cpfDoAluno | cpf | inverter }}</p>
+    <input type="text" :value="cpfDoAluno | cpf">
+    <hr>
+    <frutas />
+	</div>
+</template>
+
+<script>
+import frutas from './Frutas';
+export default {
+  components: { frutas },
+
+  filters: {
+    cpf(valor){
+      const arr = valor.split('');
+
+      arr.splice(3, 0, '.');
+      arr.splice(7, 0, '.');
+      arr.splice(11, 0, '-');
+
+      return arr.join('');
+    }
+  },
+
+  data(){
+    return {
+      cpfDoAluno: '18414910807'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 1.5rem;
+}
+</style>
---
+++ filtros-mixins/filtros-mixins-exercicios/src/Frutas.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="fruta in frutas" :key="fruta">{{ fruta }}</li>
+    </ul>
+
+    <input type="text" v-model="fruta" @keydown.enter="add">
+  </div>
+</template>
+
+<script>
+import frutasMixins from './frutasMixins';
+
+export default {
+  mixins: [frutasMixins]
+}
+</script>
