551ce61722b1538d06a4ec2096180e1faf8fd7a6
Fernando Jesus <fernandoj.5a@stelo.com.br>
2019-12-17 17:21:00 -0200

    Axios

--- http/http-exercicios/src/App.vue
+++ http/http-exercicios/src/App.vue
@@ -2,6 +2,14 @@
   <div id="app" class="container">
     <h1>HTTP com Axios</h1>
 
+    <b-alert
+      show
+      dismissible
+      v-for="mensagem in mensagens"
+      :key="mensagem.texto"
+      :variant="mensagem.tipo"
+    >{{ mensagem.texto }}</b-alert>
+
     <b-card>
       <b-form-group name="Nome">
         <b-form-input type="text" size="lg" v-model="usuario.nome" placeholder="Informe o nome"></b-form-input>
@@ -13,8 +21,29 @@
 
       <hr />
 
-      <b-button @click.prevent="salvar" variant="primary" size="lg">Salvar</b-button>
+      <b-button @click="salvar" variant="primary" size="lg">Salvar</b-button>
+
+      <b-button class="ml-2" @click="obterUsuarios" size="lg" variant="success">Obter usuarios</b-button>
     </b-card>
+
+    <hr />
+
+    <b-list-group>
+      <b-list-group-item v-for="(usuario, id) in usuarios" :key="id">
+        <strong>Nome:</strong>
+        {{ usuario.nome }}
+        <br />
+        <strong>E-mail:</strong>
+        {{ usuario.email }}
+        <br />
+        <strong>ID:</strong>
+        {{ id }}
+        <br />
+
+        <b-button variant="warning" size="lg" @click="carregar(id)">Carregar</b-button>
+        <b-button variant="danger" size="lg" class="ml-2" @click="excluir(id)">Excluir</b-button>
+      </b-list-group-item>
+    </b-list-group>
   </div>
 </template>
 
@@ -22,6 +51,9 @@
 export default {
   data() {
     return {
+      mensagens: [],
+      usuarios: [],
+      id: null,
       usuario: {
         nome: "",
         email: ""
@@ -30,8 +62,65 @@ export default {
   },
 
   methods: {
+    limpar() {
+      this.usuario.nome = "";
+      this.usuario.email = "";
+      this.id = null;
+      this.mensagens = [];
+    },
+
+    carregar(id) {
+      this.id = id;
+      this.usuario = { ...this.usuarios[id] };
+    },
+
+    excluir(id) {
+      this.$http
+        .delete(`/usuarios/${id}.json`)
+        .then(() => {
+          this.limpar();
+          this.obterUsuarios();
+        })
+        .catch(_ => {
+          this.mensagens.push({
+            texto: "problema para excluir",
+            tipo: "danger"
+          });
+        });
+    },
+
     salvar() {
-      console.log(this.usuario);
+      const metodo = this.id ? "patch" : "post";
+      const finalUrl = this.id ? `/${this.id}.json` : ".json";
+
+      this.$http[metodo](`/usuarios${finalUrl}`, this.usuario)
+        .then(() => {
+          this.limpar();
+          this.mensagens.push({
+            texto: "Operação realizada com sucesso!",
+            tipo: "success"
+          });
+        })
+        .catch(_ => {
+          this.mensagens.push({
+            texto: "problema para Salvar",
+            tipo: "danger"
+          });
+        });
+    },
+
+    obterUsuarios() {
+      this.$http
+        .get("usuarios.json")
+        .then(resp => {
+          this.usuarios = resp.data;
+        })
+        .catch(_ => {
+          this.mensagens.push({
+            texto: "problema para Obter usuários",
+            tipo: "danger"
+          });
+        });
     }
   }
   // created() {
--- http/http-exercicios/src/plugins/axios.js
+++ http/http-exercicios/src/plugins/axios.js
@@ -2,12 +2,48 @@ import Vue from "vue";
 import axios from "axios";
 
 // axios.defaults.baseURL = "https://curso-vue-4b8d6.firebaseio.com/";
+// axios.defaults.headers.common["Autorization"] = "abc123";
+// axios.defaults.headers.get["accepts"] = "aplication/json";
 
 Vue.use({
   install(Vue) {
     // Vue.prototype.$http = axios;
     Vue.prototype.$http = axios.create({
-      baseURL: "https://curso-vue-4b8d6.firebaseio.com/"
+      baseURL: "https://curso-vue-4b8d6.firebaseio.com/",
+      headers: {
+        // Autorization: "abc253" // Global
+        get: {
+          Autorization: "abc253"
+        }
+      }
     });
+
+    Vue.prototype.$http.interceptors.request.use(
+      config => {
+        // console.log(config.method);
+
+        // if (config.method === "post") {
+        //   config.method = "put";
+        // }
+
+        return config;
+      },
+      error => Promise.reject(error)
+    );
+
+    Vue.prototype.$http.interceptors.response.use(
+      res => {
+        // const array = [];
+
+        // for (let chave in res.data) {
+        //   array.push({ id: chave, ...res.data[chave] });
+        // }
+
+        // res.data = array;
+
+        return res;
+      },
+      error => Promise.reject(error)
+    );
   }
 });
